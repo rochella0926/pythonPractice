4a52b71f1c04e964789f189b9ea9d83f3ce4f38f
rochella0926 <rochella0926@gmail.com>
2021-08-10 17:17:40 +0900

    8/10

--- BEAKJUN2739.py
+++
@@ -1,7 +0,0 @@
-#BEAKJUN 2739
-#N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
-
-n=int(input())
-result=1
-for x in range(1,10):
-    print(n,"*",x,"=",n*x)
---
+++ codelion_random_lotto.py
@@ -0,0 +1,10 @@
+#codelion_random_lotto
+#정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+import random
+num=int(input())
+for i in range(num):
+    randomNumber = random.sample(range(1, 46), 6)
+    randomNumber.sort()
+    print(randomNumber)
+
